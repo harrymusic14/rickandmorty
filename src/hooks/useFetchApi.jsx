67c340df12d709b5b7f842a5e7e74aeed679e063
H.R.H <172458702+harrymusic14@users.noreply.github.com>
2025-03-21 08:12:06 -0500

    ingrese los cambios

---
+++ src/hooks/useFetchApi.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+export function useFetchApi () {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    const request = async (url) => {
+        setLoading(true)
+        setError(null)
+        try {
+            const res = await axios.get(url)
+            setData(res.data)
+        } catch (error){
+            setError(error.response?.data?.message || error.message)
+        } finally {
+            setLoading(false)   
+        }
+    }
+
+    return {data, request, loading, error}
+}
+
+export default useFetchApi
